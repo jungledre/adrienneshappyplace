c9a0713486240fb4e08f54a1051d0440bf665b04
jungledre <akerr@lmtc-adkerr.jomax.paholdings.com>
2019-12-24 16:32:30 -0800

    Add snow

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Snow from 'react-snow-effect';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Fragment>
+        <Snow />
+        <App />
+  </Fragment>
+, document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
